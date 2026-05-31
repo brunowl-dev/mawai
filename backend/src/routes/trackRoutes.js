14de8bb90316e35899657cc64b9b45af7a3bfc68
Bruno Wai Lu <brunoantoniowl@gmail.com>
2026-05-31 16:12:02 -0300

    restaura estrutura

--- backend/src/routes/trackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { listTracks, createTrack } = require('../controllers/trackController');
-
-const router = express.Router();
-
-router.get('/', listTracks);
-router.post('/', createTrack);
-
-module.exports = router;
